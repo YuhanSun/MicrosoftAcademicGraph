7d7a570ecb9a3353416f0323ac178499abf024b9
Yuhan Sun <wdmzssyh@gmail.com>
2016-01-10 13:23:00 -0700

    Temp file update

--- MapQuestExtract.py
+++ MapQuestExtract.py
@@ -2,12 +2,14 @@ __author__ = 'ysun138'
 import codecs
 
 location_set = set()
-original_input = codecs.open("D:\MicrosoftAcademicGraph_2015_11\Conference_instance_coordinates.csv",'r',"utf-8")
+# original_input = codecs.open("D:\MicrosoftAcademicGraph_2015_11\Conference_instance_coordinates.csv",'r',"utf-8")
+original_input = codecs.open("D:\MicrosoftAcademicGraph_2015_11\Affiliations_coordinate.csv",'r',"utf-8")
 for line in original_input:
     location_set.add(line.split("\t")[0])
 original_input.close()
 
-inputfile = codecs.open("MapQuest_conference.gpx",'r',"utf-8")
+# inputfile = codecs.open("MapQuest_conference.gpx",'r',"utf-8")
+inputfile = codecs.open("MapQuest_Affiliation.gpx",'r',"utf-8")
 outfile = codecs.open("temp.gpx","a","utf-8")
 
 inputfile.readline()
